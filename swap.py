d055692d9c1614ec2846e524051b36a64b948ba3
ranjuk94 <ranjuramakrishnan906@gmail.com>
2024-08-12 10:41:28 +0530

    second commit

---
+++ swap.py
@@ -0,0 +1,10 @@
+x = int(input("enter a number:"))
+y = int(input("enter a number:"))
+#a = x
+#x = y
+#y = a
+x = x + y
+y = x - y
+x = x - y
+print(x,y)
+
---
+++ vote.py
@@ -0,0 +1,5 @@
+x = int(input("enter your age:"))
+if x >= 18:
+    print("You are Eligible to vote")
+else:
+    print("you are not eligible to vote")
