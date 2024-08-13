48094c3b75041da29ef81cc99bc3c85e173bb574
ranjuk94 <ranjuramakrishnan906@gmail.com>
2024-08-13 11:44:18 +0530

    second commit

---
+++ convert.py
@@ -0,0 +1,3 @@
+x=int(input("Enter a value in kilometers:"))
+x = x * 0.621371
+print("The value in mile:",x)
---
+++ convertcelsius.py
@@ -0,0 +1,3 @@
+x = int(input("Enter a value in celsius:"))
+x = x * (9/5)+32
+print("The value in Fahrenheit:",x)
